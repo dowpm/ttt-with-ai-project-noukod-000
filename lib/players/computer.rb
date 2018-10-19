bd5295d6ad4028c194e70f41a44bfffeea71606e
Mac Dowell Prosper <macdowell@noukod.com>
2018-10-19 05:57:26 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,8 +1,14 @@
 module Players
   class Computer < Player
 
+    def initialize (token)
+      super
+      @m = (1..9).to_a
+    end
+
     def move board
-      rand(9-board.turn_count).to_s
+      m = rand(9-board.turn_count).to_i
+      (1..9)
     end
   end
 
