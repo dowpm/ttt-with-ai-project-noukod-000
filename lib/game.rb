e55c4c51dc7468987434745e226ec876e66c04ca
Mac Dowell Prosper <macdowell@noukod.com>
2018-10-19 05:12:50 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -49,7 +49,9 @@ class Game
       turn
     else
       puts "\nTurn: #{@board.turn_count+1}\n"
+      # board.display
       board.update input, current_player
+      puts "Player #{current_player} move to position #{input}"
       board.display
       puts "\n"
     end
